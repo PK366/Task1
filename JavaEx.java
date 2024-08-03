7ba54caa53b67269a6be8000da1b86afc9530d6f
PRAMOD KUMAR <114461664+PK366@users.noreply.github.com>
2024-08-03 17:22:33 +0530

    Create JavaEx.java

---
+++ JavaEx.java
@@ -0,0 +1,20 @@
+public class JavaEx {
+	public static void main(String[] args) {
+		String firstName = "Pramod";
+		String lastName = "Kumar";
+		String fullName = firstName + lastName;
+		int x = 15;
+		int y = 25;
+		int z = x + y;
+		int a = z * 5;
+		boolean isRaining = true ;
+		boolean isTodayMonday = false;
+		//Full Name....
+		System.out.println(fullName);
+		System.out.println(a);
+		System.out.println(z);
+		System.out.println("Is Raining?: " + isRaining);
+		System.out.println("Is today Monday? :" + isTodayMonday);
+		System.out.println("This project is commited successfully");
+	}
+}
